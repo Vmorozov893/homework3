7463ad5b0393486ecddeced4e3cc7b0b8f2a82c8
vmorozov893 <vmorozov893@gmail.com>
2024-07-19 13:35:36 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Задание 1\n");
 
+        int age = 19;
+        if(age >= 18){
+            System.out.println("Вы достигли совершеннолетия.");
+        }else{
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно немного подождать.");
+        }
+
+        System.out.println("Задание 2\n");
 
     }
 }
